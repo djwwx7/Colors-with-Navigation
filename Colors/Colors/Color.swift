4e4c6f2a131cd39c82c6bd217087b4464ca51de9
djwwx7 <djwwx7@mail.missouri.edu>
2017-07-21 16:26:35 -0500

    Added navigation

---
+++ Colors/Colors/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Colors
+//
+//  Created by Whitaker, Cody (MU-Student) on 7/21/17.
+//  Copyright © 2017 Whitaker, Cody (MU-Student). All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
